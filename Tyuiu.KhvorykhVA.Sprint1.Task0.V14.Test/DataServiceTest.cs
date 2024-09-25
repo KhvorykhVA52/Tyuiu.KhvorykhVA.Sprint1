6b15b969c1e7be404185b16f4cef81354f192b47
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-09-25 18:29:25 +0500

    Task0

---
+++ Tyuiu.KhvorykhVA.Sprint1.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KhvorykhVA.Sprint1.Task0.V14.Lib;
+
+namespace Tyuiu.KhvorykhVA.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(25, res);
+
+        }
+    }
+}
---
+++ Tyuiu.KhvorykhVA.Sprint1.Task0.V14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KhvorykhVA.Sprint1.Task0.V14.Lib;
+
+
+
+namespace Tyuiu.KhvorykhVA.Sprint1.Task0.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Хыорых В. А. | АСОиУБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Хворых Виктор Александрович | АСОиУБ-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 2 * 3 * 3 + 7           *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2 * 3 * 3 + 7                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+
+        }
+    }
+}
