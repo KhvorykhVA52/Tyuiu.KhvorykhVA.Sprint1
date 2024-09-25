8d81743bb45d73e565f3cb1dfaea671268f29ed0
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-09-25 18:52:26 +0500

    Task2

---
+++ Tyuiu.KhvorykhVA.Sprint1.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KhvorykhVA.Sprint1.Task2.V24.Lib;
+namespace Tyuiu.KhvorykhVA.Sprint1.Task2.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 3;
+            var res = ds.CalculateDiffSquare(x, y);
+            Assert.AreEqual(9, res);
+        }
+    }
+}
---
+++ Tyuiu.KhvorykhVA.Sprint1.Task2.V24/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KhvorykhVA.Sprint1.Task2.V24.Lib;
+
+namespace Tyuiu.KhvorykhVA.Sprint1.Task2.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Квадрат разницы = " + ds.CalculateDiffSquare(x, y));
+            Console.ReadKey();
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
