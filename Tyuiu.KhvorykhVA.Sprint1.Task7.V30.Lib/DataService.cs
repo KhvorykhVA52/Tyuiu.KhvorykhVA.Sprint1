a5b7b4b5ded0de0aeea8fde467fcfd7eb2d40fc5
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-10-02 15:09:18 +0500

    Task7

--- Tyuiu.KhvorykhVA.Sprint1.Task7.V30.Lib/DataService.cs
+++ Tyuiu.KhvorykhVA.Sprint1.Task7.V30.Lib/DataService.cs
@@ -5,13 +5,22 @@ namespace Tyuiu.KhvorykhVA.Sprint1.Task7.V30.Lib
     {
         public double Calculate(double x, double y)
         {
-            double firstPart = x + Math.Exp(x);
 
-            double secondPart = (Math.Sin(Math.Pow(x, 5)) + Math.Pow(x, 3)) / Math.Pow(3, x);
+            double firstPart = x + Math.Exp(x);
+            Console.WriteLine($"Первая часть: {firstPart}");
+            double sinX5 = Math.Sin(Math.Pow(x, 5)); 
+            double xCubed = Math.Pow(x, 3); 
+            double threePowX = Math.Pow(3, x);
+            double secondPart = (sinX5 + xCubed) / threePowX;
+            Console.WriteLine($"sin(x^5): {sinX5}, x^3: {xCubed}, 3^x: {threePowX}");
+            Console.WriteLine($"Вторая часть: {secondPart}");
 
             double thirdPart = Math.Pow(y, 5) / (5 * y);
+            Console.WriteLine($"Третья часть: {thirdPart}");
 
             double z = firstPart + secondPart + thirdPart;
+            Console.WriteLine($"Результат до округления: {z}");
+
             return Math.Round(z, 3);
         }
     }
